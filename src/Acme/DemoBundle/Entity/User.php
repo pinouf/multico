624732fa9628f7e76eb2c477be5a782c5f126489
Jean-Marc Chieng <chieng_jeanmarc@yahoo.fr>
2014-09-19 17:22:57 +0200

    toto

--- src/Acme/DemoBundle/Entity/User.php
+++ src/Acme/DemoBundle/Entity/User.php
@@ -1,35 +1,27 @@
-<?php 
+<?php
+// src/Acme/UserBundle/Entity/User.php
 
 namespace Acme\DemoBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
- *
- * @ORM\Table(name="user")
  * @ORM\Entity
+ * @ORM\Table(name="user")
  */
-class User
+class User extends BaseUser
 {
     /**
-     * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $user_id;
+    protected $id;
 
-    /**
-     * @var string $ppluid
-     * @ORM\Column(name="PPL_UID", type="string", length=255, nullable=true)
-     */
-    protected $pplUid;
-
-    /**
-     * @var string $pplFirstName
-     * @ORM\Column(name="PPL_FIRSTNAME", type="string", length=255, nullable=true)
-     */
-    protected $pplFirstName;
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
